3f0210593bf5d68c40aeb029355ba56519a53d05
Sean Genabe <seangenabe@outlook.com>
2016-08-10 06:49:15 +0800

    fix test

--- test/test.test.js
+++ test/test.test.js
@@ -14,12 +14,15 @@ t(async t => {
     })
   })
   let expected = FS.readFileSync(`${__dirname}/expected1.js`, 'utf8')
-  result = win32compat(s)
-  expected = win32compat(s)
+  result = win32compat(result)
+  expected = win32compat(expected)
   t.is(result, expected)
 })
 
 function win32compat(s) {
+  if (Buffer.isBuffer(s)) {
+    s = s.toString()
+  }
   // For some reason, stream replacers that I find ignore \r.
   // Well, whatever. Let's just normalize our strings.
   return s.split('\r').join('')
